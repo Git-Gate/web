93f166d719baf89094f9a069d944be5503b63bae
PaoloRollo <paolo.rollo1997@gmail.com>
2022-10-28 22:18:59 +0100

    update.

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -48,21 +48,32 @@ export default function NewRepository() {
             {
                 selectedRepo && <div className="py-8">
                     <RequirementsTable />
-                    <div className="sm:flex sm:items-center">
-                        <div className="sm:flex-auto">
-                        <h2 className="text-2xl font-semibold text-white">Blacklisted addresses</h2>
-                        <p className="mt-2 text-sm text-gray-400">
-                            Comma separated value format.
-                        </p>
+                    <div className="p-8">
+                        <div className="sm:flex sm:items-center">
+                            <div className="sm:flex-auto">
+                            <h2 className="text-2xl font-semibold text-white">Blacklisted addresses</h2>
+                            <p className="mt-2 text-sm text-gray-400">
+                                Comma separated value format.
+                            </p>
+                            </div>
+                            <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
+                            <button
+                                type="button"
+                                className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+                            >
+                                Upload CSV
+                            </button>
+                            </div>
                         </div>
-                        <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                        <button
-                            type="button"
-                            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-                        >
-                            Upload CSV
-                        </button>
-                        </div>
+                        <textarea
+                            rows={5}
+                            name="comment"
+                            id="comment"
+                            className="block w-full md:w-1/2 resize-none border-0 py-3 focus:ring-0 sm:text-sm mt-4 px-2"
+                            placeholder="0x000000..."
+                            defaultValue={''}
+                        />
+
                     </div>
                 </div>
             }
